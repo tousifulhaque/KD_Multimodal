360804760c88e2a46c3f86a48aaa108297926503
Syed Tousiful Haque <tousifulhaque@iut-dhaka.edu>
2022-10-25 15:55:46 +0000

    Feat: Working test file

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 from model import Transformer
-from sklearn import train_test_split
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    classification_report,
+    confusion_matrix,
+    ConfusionMatrixDisplay,
+)
+import numpy as np
+import matplotlib.pyplot as plt
 
+def get_predictions(start, end):
+    out= model(X_test[start:end])
+    predictions = np.argmax(out, axis=-1)
+
+    return predictions
 
 if __name__  == "__main__":
     CLASS_LABELS = np.array(
@@ -47,4 +60,42 @@ if __name__  == "__main__":
     num_classes=18,
     dropout_rate=0.0,
     attention_dropout_rate=0.0,
-)
+    )
+
+    out = model(tf.expand_dims(X_test[20], axis=0))
+    print(out.shape)
+    model.load_weights("model-best.h5")
+    batch_size = 256  # set it by your GPU size
+
+    full_predictions = []
+    for i in range(X_test.shape[0] // batch_size):
+        y = get_predictions(i * batch_size, (i + 1) * batch_size)
+        full_predictions.append(y)
+
+    y = get_predictions((i + 1) * batch_size, X_test.shape[0])
+    full_predictions.append(y)
+
+    full_predictions = np.concatenate(full_predictions, axis=0)
+    print(full_predictions.shape)
+
+    full_predictions = full_predictions.reshape(-1, full_predictions.shape[-1])
+    print(full_predictions.shape)
+
+    fig, ax = plt.subplots(figsize=(15, 15))
+    cm = confusion_matrix(
+        CLASS_LABELS[y_test.reshape(-1)],
+        CLASS_LABELS[full_predictions.reshape(-1)],
+        labels=CLASS_LABELS,
+    )
+    cm_display = ConfusionMatrixDisplay(
+        confusion_matrix=cm, display_labels=CLASS_LABELS
+    ).plot(cmap="Blues", xticks_rotation=70, ax=ax)
+
+    print(
+        classification_report(
+            CLASS_LABELS[y_test.reshape(-1)],
+            CLASS_LABELS[full_predictions.reshape(-1)],
+            labels=CLASS_LABELS,
+            digits=3,
+        )
+    )
