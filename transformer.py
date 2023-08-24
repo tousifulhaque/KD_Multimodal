f562c20af37ad11165d0c648204a921d2805d55e
Tousif <tousifulhaque@iut-dhaka.edu>
2023-08-24 18:09:16 -0500

    Feat: Sliding Window

---
+++ transformer.py
@@ -0,0 +1,62 @@
+
+#tensorflow imports 
+from tensorflow import keras
+import tensorflow as tf
+from tensorflow.keras.layers import Add, Dense, LayerNormalization,GlobalAveragePooling1D
+from tensorflow.keras.layers import Conv1D, Dropout, MultiHeadAttention, Layer, Embedding
+from tensorflow.keras.initializers import TruncatedNormal
+
+#local package import 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# function to get sinusoidal positional embedding
+def get_positional_embedding(seq_len,d_model, n = 10000):
+    P = np.zeros((seq_len, d_model))
+    for k in range(seq_len):
+        for i in np.arange(int(d_model/2)):
+            denominator = np.power(n, 2*i/d_model)
+            P[k, 2*i] = np.sin(k/denominator)
+            P[k, 2*i + 1] = np.cos(k/denominator)
+    P = P[np.newaxis, : ,:]
+    return tf.Variable(P,trainable = False ,dtype = tf.float32)
+
+def encoder(x, embed_dim, mlp_dim, num_heads, dropout_rate, attention_dropout_rate, length, channel):
+    
+    #attention_layer
+    y = LayerNormalization(epsilon = 1e-6)(x)
+    y = MultiHeadAttention(num_heads = num_heads,key_dim = embed_dim,dropout = attention_dropout_rate, kernel_initializer = TruncatedNormal(stddev = 0.02))(query = x,value = x,key = x,training = True)
+    y = Dropout(rate = dropout_rate)(y)
+    y = Add()([x,y])
+    
+    #mlp_layer
+    y = LayerNormalization(epsilon = 1e-6)(x)
+    y = Dense(units = mlp_dim, kernel_initializer = TruncatedNormal(stddev = 0.02))(y)
+    y = Dropout(rate = dropout_rate)(y)
+    y = Dense(units = embed_dim, kernel_initializer = TruncatedNormal(stddev = 0.02))(y)
+    y = Dropout(rate = dropout_rate)(y)
+    y = Add()([x, y])
+    
+    return y
+
+def transformer(length, channels,num_layers, embed_dim, mlp_dim, num_heads, dropout_rate, attention_dropout_rate):
+    
+    #initial normalization
+    pos_embed = get_positional_embedding(length, embed_dim)
+    inputs= keras.Input(shape = (length, channels))
+    x = Dense(embed_dim)(inputs)
+    #x = Normalization()(inputs)
+    x = x + pos_embed
+    #stacking encoder layers
+    for _ in range(num_layers):
+        x = encoder(x,embed_dim, mlp_dim, num_heads, dropout_rate, attention_dropout_rate, length,channels)
+    x = LayerNormalization(epsilon=1e-5)(x)
+    
+    #pooling
+    x = GlobalAveragePooling1D(data_format = 'channels_first')(x)
+    
+    #output
+    output = Dense(1, kernel_initializer="zeros", activation = 'sigmoid')(x)
+    
+    return keras.Model(inputs, output)
---
+++ utils.py
@@ -0,0 +1,100 @@
+#python libs
+import re 
+import math
+#import pandas as pd
+import numpy as np
+#from tensorflow.keras import layers
+
+#local libs
+import cfg
+
+def normalize(data, np_array=False, scaled=False):
+    """
+    Normalize and optionally scale a dataset.
+    
+    @param (DataFrame or numpy array) data: 2D DataFrame or numpy array
+    @param (bool) np_array : optional np_array flag (default = false)
+                             forces return type to numpy array
+    @param (bool) scale : optionally scale the dataset (default = false)
+
+    Return: Pandas DataFrame or Numpy array with normalized/scaled data
+    """
+    # ensure floats
+    data = data.astype(float)
+    
+    if detect_datatype(data) == DataType.NUMPY:
+        # set-up normalization
+        high = 1.0
+        low = 0.0
+        mins = np.min(data, axis=0)
+        maxs = np.max(data, axis=0)
+        rng = maxs - mins
+        # normalize
+        data = high - (((high - low) * (maxs - data)) / rng)
+        # scale if needed
+        if scaled:
+            data = (data / .5) - 1
+    elif detect_datatype(data) == DataType.DATAFRAME:
+        # normalize
+        [data[col].update((data[col] - data[col].min()) / (data[col].max() - data[col].min())) for col in data.columns]
+        # scale if needed
+        if scaled:
+            [data[col].update((data[col] / .5)-1) for col in data.columns]
+    
+    # return appropriate object
+    if np_array and detect_datatype(data) == DataType.DATAFRAME:
+        data = data.to_numpy()
+    elif not np_array and detect_datatype(data) == DataType.NUMPY:
+        data = pd.DataFrame(data)
+    
+    return data
+
+def shape_3D_data(data, timesteps):
+    if len(data.shape) != 2: raise TypeError("Input data must be 2D.")
+    
+    """
+    Resape 2D data into 3D data of groups of 2D timesteps
+    
+    @param (DataFrame or numpy array) data: 2D DataFrame or numpy array
+    @param (int) timesteps: number of timesteps/group
+
+    Return: The reshaped data as numpy 3D array
+    """ 
+
+    # samples are total number of input vectors
+    samples = data.shape[0]
+    # time steps are steps per batch
+    features = data.shape[1]
+    
+    # samples must divide evenly by timesteps to create an even set of batches
+    if not(samples % timesteps):
+        return np.array(data).reshape(int(data.shape[0] / timesteps), timesteps, features)
+    else:
+        msg = "timesteps must divide evenly into total samples: " + str(samples) + "/" \
+            + str(timesteps) + "=" + str(round(float(samples) / float(timesteps), 2))
+        raise ValueError(msg)
+
+def sliding_window(data,labels, clearing_time_index, max_time, sub_window_size, stride_size):
+
+    assert clearing_time_index >= sub_window_size - 1 , "Clearing value needs to be greater or equal to (window size - 1)"
+    start = clearing_time_index - sub_window_size + 1 
+
+    if max_time >= data.shape[0]-sub_window_size:
+        max_time = max_time - sub_window_size + 1
+        # 2510 // 100 - 1 25 #25999 1000 24000 = 24900
+
+    sub_windows  = (
+        start + 
+        np.expand_dims(np.arange(sub_window_size), 0) + 
+        np.expand_dims(np.arange(max_time, step = stride_size), 0).T
+    )
+    print(sub_windows.shape)
+    labels = np.round(np.mean(labels[sub_windows], axis=1))
+    return data[sub_windows], labels
+
+
+if __name__ == "__main__":
+    data = np.random.randn(79955, 3)
+    labels = np.random.randn(79955)
+    new_data  = sliding_window(data, labels, 99, 10034, 100, 100)
+    print(new_data.shape)
