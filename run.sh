7e8922232f60d7daf4ba0fb6f3c0aae495da4ce5
Tousif <tousifulhaque@iut-dhaka.edu>
2023-08-30 18:22:52 -0500

    Feat: Test commit

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#test
 python3 processing.py --source '/Users/tousif/Lstm_transformer/KD_Multimodal/dataset/Labelled_Student_data/val' \
                         --dest '/Users/tousif/Lstm_transformer/KD_Multimodal/dataset/Labelled_Student_data/val/ADL' \
                         --data-type 'phone&watch'\
